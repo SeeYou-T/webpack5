e1b9476e7a8fc8d165fbdb8c6f4295cf08772121
yunkai.tan <yunkai.tan@tarsocial.com>
2021-12-09 17:26:12 +0800

    feat: code_Splitting

---
+++ 08_code_Splitting/dist/math.bundle.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["math"],{
+
+/***/ "./src/math.js":
+/*!*********************!*\
+  !*** ./src/math.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "add": () => (/* binding */ add),
+/* harmony export */   "minus": () => (/* binding */ minus)
+/* harmony export */ });
+function add(x, y) {
+  return x + y;
+}
+function minus(x, y) {
+  return x - y;
+}
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWF0aC5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7QUFBTyxTQUFTQSxHQUFULENBQWFDLENBQWIsRUFBZ0JDLENBQWhCLEVBQW1CO0FBQ3hCLFNBQU9ELENBQUMsR0FBR0MsQ0FBWDtBQUNEO0FBQ00sU0FBU0MsS0FBVCxDQUFlRixDQUFmLEVBQWtCQyxDQUFsQixFQUFxQjtBQUMxQixTQUFPRCxDQUFDLEdBQUdDLENBQVg7QUFDRCIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9tYXRoLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBhZGQoeCwgeSkge1xyXG4gIHJldHVybiB4ICsgeTtcclxufVxyXG5leHBvcnQgZnVuY3Rpb24gbWludXMoeCwgeSkge1xyXG4gIHJldHVybiB4IC0geTtcclxufVxyXG4iXSwibmFtZXMiOlsiYWRkIiwieCIsInkiLCJtaW51cyJdLCJzb3VyY2VSb290IjoiIn0=
---
+++ 08_code_Splitting/src/math.js
@@ -0,0 +1,6 @@
+export function add(x, y) {
+  return x + y;
+}
+export function minus(x, y) {
+  return x - y;
+}
